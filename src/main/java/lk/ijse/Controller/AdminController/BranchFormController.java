361f29d9cb9e88ffb55076fa300dc4b26b104554
danukarangith <danukarangith456@gmail.com>
2024-03-14 19:19:49 +0530

    BranchFormController implemneted

--- src/main/java/lk/ijse/Controller/AdminController/BranchFormController.java
+++ src/main/java/lk/ijse/Controller/AdminController/BranchFormController.java
@@ -1,4 +1,53 @@
 package lk.ijse.Controller.AdminController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import lk.ijse.Bo.BranchService;
+import lk.ijse.Bo.ServiceFactor;
+import lk.ijse.Controller.Table_Row.BranchRowController;
+import lk.ijse.Dto.BranchDto;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class BranchFormController {
+    public VBox TableBox;
+
+    BranchService branchBo = (BranchService) ServiceFactor.getBoFactory().getBo(ServiceFactor.BoType.Branch);
+
+    public void AddUserOnActhion(ActionEvent actionEvent) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Forms/Table_Row/Save/BranchSave.fxml"));
+        try {
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initialize() {
+        laodTable();
+    }
+
+    private void laodTable() {
+        TableBox.getChildren().clear();
+        ArrayList<BranchDto> all = branchBo.getAll();
+        for (BranchDto branchDto : all) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Forms/Table_Row/BranchRow.fxml"));
+                Parent root = fxmlLoader.load();
+                BranchRowController controller = fxmlLoader.getController();
+                controller.setData(branchDto);
+                TableBox.getChildren().add(root);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
