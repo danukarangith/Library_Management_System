96c16bb7a1838077c3dd9e4a1df1dacf55f4d4c6
danukarangith <danukarangith456@gmail.com>
2024-03-14 17:13:55 +0530

    book view form implemented

---
+++ src/main/java/lk/ijse/Controller/Table_Row/View/ViewBookFormController.java
@@ -0,0 +1,21 @@
+package lk.ijse.Controller.Table_Row.View;
+
+import javafx.scene.control.Label;
+import lk.ijse.Dto.BookDto;
+
+public class ViewBookFormController {
+    public Label Book_Id;
+    public Label available;
+    public Label title;
+    public Label author;
+    public Label genre;
+
+    public void setData(BookDto bookDto){
+        Book_Id.setText("B0"+bookDto.getId());
+        available.setText(bookDto.getAvailable());
+        title.setText(bookDto.getTitle());
+        author.setText(bookDto.getAutor());
+        genre.setText(bookDto.getGenre());
+        author.setText(bookDto.getGenre());
+    }
+}
