4a02b8e44ead876f3dbccdf8dbf57a9b698f15cf
danukarangith <danukarangith456@gmail.com>
2024-03-14 19:22:03 +0530

    implementaion update

--- src/main/java/lk/ijse/Controller/Table_Row/Save/SaveBranchController.java
+++ src/main/java/lk/ijse/Controller/Table_Row/Save/SaveBranchController.java
@@ -1,4 +1,34 @@
 package lk.ijse.Controller.Table_Row.Save;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import lk.ijse.Bo.BranchService;
+import lk.ijse.Bo.ServiceFactor;
+import lk.ijse.Dto.BranchDto;
+
 public class SaveBranchController {
+    public TextField name;
+    public TextField locationtext;
+    public TextField email;
+    BranchService branchService = (BranchService) ServiceFactor.getBoFactory().getBo(ServiceFactor.BoType.Branch);
+
+    @FXML
+    public void saveBtnOnActhion(ActionEvent actionEvent) { // Corrected typo in method name
+//        if(Regex.name(name.getText()) && Regex.city(locationtext.getText()) && Regex.email(email.getText())){
+        int save = branchService.save(new BranchDto(0, name.getText(), locationtext.getText(), email.getText()));
+        if (save > 0) {
+            name.clear();
+            locationtext.clear();
+            email.clear();
+            new Alert(Alert.AlertType.CONFIRMATION, "Saved..").show();
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Try Again..").show();
+        }
+//        }
+//        else {
+//            new Alert(Alert.AlertType.ERROR, "Invalid Input").show();
+//        }
+    }
 }
