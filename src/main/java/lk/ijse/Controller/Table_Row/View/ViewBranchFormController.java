e9dce648da36a138d724f5e874290cb1b6a860fd
danukarangith <danukarangith456@gmail.com>
2024-03-14 18:52:46 +0530

    branch view implemneted

--- src/main/java/lk/ijse/Controller/Table_Row/View/ViewBranchFormController.java
+++ src/main/java/lk/ijse/Controller/Table_Row/View/ViewBranchFormController.java
@@ -1,4 +1,26 @@
 package lk.ijse.Controller.Table_Row.View;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import lk.ijse.Dto.BranchDto;
+
 public class ViewBranchFormController {
+    @FXML
+    private TextField Branch_Id;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField locationtext;
+
+    @FXML
+    private TextField name;
+
+    public void setData(BranchDto branchDto){
+        Branch_Id.setText("B0"+branchDto.getId());
+        name.setText(branchDto.getName());
+        locationtext.setText(branchDto.getLocation());
+        email.setText(branchDto.getEmail());
+    }
 }
