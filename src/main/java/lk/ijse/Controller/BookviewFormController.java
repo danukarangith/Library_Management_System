d877aef8c77976bc43035db4b2aab747fef98c9f
danukarangith <danukarangith456@gmail.com>
2024-03-14 21:32:42 +0530

    book view page implemented

--- src/main/java/lk/ijse/Controller/BookviewFormController.java
+++ src/main/java/lk/ijse/Controller/BookviewFormController.java
@@ -1,4 +1,80 @@
 package lk.ijse.Controller;
 
-public class BookviewFormController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import lk.ijse.Bo.SearchBookService;
+import lk.ijse.Bo.ServiceFactor;
+import lk.ijse.Dto.BookDto;
+import org.controlsfx.control.textfield.AutoCompletionBinding;
+import org.controlsfx.control.textfield.TextFields;
+
+import java.net.URL;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+public class BookviewFormController implements Initializable {
+
+    @FXML
+    private TextField AutorText;
+
+    @FXML
+    private TextArea DisText;
+
+    @FXML
+    private TextField genreText;
+
+    @FXML
+    private Label lable;
+
+    @FXML
+    private Text lableText;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private TextField titleText;
+
+    SearchBookService searchBookService = (SearchBookService) ServiceFactor.getBoFactory().getBo(ServiceFactor.BoType.Search_Book);
+
+    @FXML
+    void searchOnActhion(ActionEvent event) {
+
+        BookDto bookDto = searchBookService.getData(searchBar.getText());
+
+        titleText.setText(bookDto.getTitle());
+        AutorText.setText(bookDto.getAutor());
+        DisText.setText(bookDto.getDis());
+        genreText.setText(bookDto.getGenre());
+
+        if(bookDto.getAvailable().equals("yes")){
+            lable.setStyle("-fx-background-color: green");
+            lableText.setText("Yes");
+        }
+        else {
+            lable.setStyle("-fx-background-color: red");
+            lableText.setText("No");
+        }
+
+    }
+
+    private AutoCompletionBinding<String> Autotitle;
+
+    private List<String> titles = searchBookService.getTitles();
+
+    private Set<String> titleSet = new HashSet<>(titles);
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        Autotitle = TextFields.bindAutoCompletion(searchBar,titleSet);
+
+    }
 }
