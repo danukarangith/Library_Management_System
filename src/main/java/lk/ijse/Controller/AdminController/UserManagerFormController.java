7e24cb544ad8f9ca1ceaa117013f23dd83792bcb
danukarangith <danukarangith456@gmail.com>
2024-03-14 20:18:15 +0530

    User Manage implemented

--- src/main/java/lk/ijse/Controller/AdminController/UserManagerFormController.java
+++ src/main/java/lk/ijse/Controller/AdminController/UserManagerFormController.java
@@ -1,4 +1,39 @@
 package lk.ijse.Controller.AdminController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
+import lk.ijse.Bo.ServiceFactor;
+import lk.ijse.Bo.UserManageService;
+import lk.ijse.Controller.Table_Row.UserRowController;
+
+import java.io.IOException;
+
 public class UserManagerFormController {
+    public VBox TableBox;
+
+    UserManageService userMangeBo = (UserManageService) ServiceFactor.getBoFactory().getBo(ServiceFactor.BoType.UserMange);
+    public void initialize() {
+        TableBox.getChildren().clear();
+        userMangeBo.getAll().forEach(MemberDto -> {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Forms/Table_Row/UserRow.fxml"));
+                Parent root = fxmlLoader.load();
+                UserRowController controller = fxmlLoader.getController();
+                controller.setData(MemberDto);
+                TableBox.getChildren().add(root);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void AddUserOnActhion(ActionEvent actionEvent) {
+
+    }
+
+    public void UpdateuserOnActhion(ActionEvent actionEvent) {
+
+    }
 }
--- src/main/java/lk/ijse/Controller/Table_Row/UserRowController.java
+++ src/main/java/lk/ijse/Controller/Table_Row/UserRowController.java
@@ -1,4 +1,36 @@
 package lk.ijse.Controller.Table_Row;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import lk.ijse.Bo.ServiceFactor;
+import lk.ijse.Bo.UserManageService;
+import lk.ijse.Dto.MemberDto;
+
 public class UserRowController {
+    public Label UserId;
+    public Label name;
+    public Label Username;
+    public Label Password;
+
+    MemberDto memberDto = null;
+
+    public void setData(MemberDto userDto){
+        this.UserId.setText("Bo"+userDto.getId());
+        name.setText(userDto.getFull_name());
+        Username.setText(userDto.getUsername());
+        Password.setText(userDto.getPassword());
+        memberDto = userDto;
+    }
+
+    UserManageService userMangeBo = (UserManageService) ServiceFactor.getBoFactory().getBo(ServiceFactor.BoType.UserMange);
+
+    public void DeleteBtnOnActhion(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to delete this user?", ButtonType.YES, ButtonType.NO);
+        alert.showAndWait();
+        if (alert.getResult() == ButtonType.YES) {
+            userMangeBo.delete(memberDto.getId());
+        }
+    }
 }
