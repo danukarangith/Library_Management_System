bfd79801e928c1fb2feb467295eb9f8e75da9a0b
danukarangith <danukarangith456@gmail.com>
2024-03-15 13:29:52 +0530

    return book transaction implemented

--- src/main/java/lk/ijse/Controller/AdminController/ReturnBookFormController.java
+++ src/main/java/lk/ijse/Controller/AdminController/ReturnBookFormController.java
@@ -1,4 +1,82 @@
 package lk.ijse.Controller.AdminController;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import lk.ijse.Bo.ReturnBookService;
+import lk.ijse.Bo.ServiceFactor;
+import lk.ijse.Entity.BorrowBook;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
 public class ReturnBookFormController {
+    @FXML
+    private TextField BookCount;
+
+    @FXML
+    private TextField Date;
+
+    @FXML
+    private ComboBox<String> MemberIdCombo;
+
+    @FXML
+    private TextField Payment;
+
+    @FXML
+    private TextField ReturnData;
+
+    public final ReturnBookService returnBookServiceI = (ReturnBookService) ServiceFactor.getBoFactory().getBo(ServiceFactor.BoType.Return_Book);
+
+    public void initialize(){
+        List<Integer> allId = returnBookServiceI.getAllId();
+        ObservableList<String> items = FXCollections.observableArrayList();
+        for (Integer integer : allId) {
+            items.add("M0"+integer);
+        }
+        MemberIdCombo.setItems(items);
+    }
+
+    BorrowBook borrowBook;
+
+    @FXML
+    void returnBtnOnActhion(ActionEvent event) {
+        borrowBook.setPayment(Double.parseDouble(Payment.getText()));
+        boolean returned = returnBookServiceI.returnBook(borrowBook);
+
+        if(returned){
+            new Alert(Alert.AlertType.CONFIRMATION,"Book Returned").show();
+        }
+    }
+
+    @FXML
+    void ComboOnActhion(ActionEvent event) {
+        BorrowBook pendingData = returnBookServiceI.getPendingData(MemberIdCombo.getValue());
+        BookCount.setText(pendingData.getQty() + "");
+        borrowBook = pendingData;
+
+        String date = String.valueOf(pendingData.getAddedDate());
+
+        String[] s = date.split(" ");
+        Date.setText(s[0]);
+
+        ReturnData.setText(pendingData.getDueDate() + "");
+        payment();
+    }
+
+    void payment(){
+        int payment = 0;
+        LocalDate date = LocalDate.now();
+        LocalDate dueDate = LocalDate.parse(ReturnData.getText());
+        long between = ChronoUnit.DAYS.between(dueDate,date);
+        if (between > 0){
+            payment = (int) (between * 100);
+        }
+        Payment.setText(payment + "");
+    }
 }
--- src/main/java/lk/ijse/Controller/ReturnBookPageController.java
+++ src/main/java/lk/ijse/Controller/ReturnBookPageController.java
@@ -1,4 +1,53 @@
 package lk.ijse.Controller;
 
-public class ReturnBookPageController {
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+import lk.ijse.Bo.ReturnBookService;
+import lk.ijse.Bo.ServiceFactor;
+import lk.ijse.Controller.Table_Row.HisBookRowFormController;
+import lk.ijse.Entity.Book_Transaction;
+import lk.ijse.Entity.BorrowBook;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class ReturnBookPageController implements Initializable {
+    private final ReturnBookService returnBookServiceI = (ReturnBookService) ServiceFactor.getBoFactory().getBo(ServiceFactor.BoType.Return_Book);
+    public VBox Vbox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        LoadTable();
+    }
+
+    void LoadTable(){
+        List<BorrowBook> allTableData = returnBookServiceI.getAllTableData();
+        System.out.println(allTableData.size()+"==============================aaaaaaaa============================");
+        for (int i = 0; i < allTableData.size(); i++) {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Forms/Table_Row/BookHis.fxml"));
+            try {
+                Vbox.getChildren().add(fxmlLoader.load());
+                Vbox.setSpacing(5);
+                Vbox.setPadding(new javafx.geometry.Insets(5, 5, 5, 5));
+                HisBookRowFormController controller = fxmlLoader.getController();
+                List<Book_Transaction> details = allTableData.get(i).getDetails();
+
+                for (int j = 0; j < details.size(); j++) {
+                    lk.ijse.Entity.Books book = details.get(j).getBook();
+                    controller.setData(
+                            "B0"+book.getId(),
+                            book.getTitle(),
+                            allTableData.get(i).getStatus()
+                    );
+                }
+                System.out.println("+++++++++++======================================================================");
+                Vbox.getChildren().add(fxmlLoader.load());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
